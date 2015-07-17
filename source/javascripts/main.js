12714552d2ee19ca20565d8d286a1dc83d5c83c8
Jelena <jelena@byteout.com>
2015-07-17 16:47:00 +0200

    content changes

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -14,11 +14,11 @@
       ['10/2003', '2006', 'At least had fun', 'yellow', ''],
       ['02/2005', '05/2006', 'Enjoyed those times as well', 'green', '#'],
       ['07/2005', '09/2005', 'Bad luck again', 'purple', '#'],
-      ['10/2005', '2008', 'For a long time nothing happened', 'red', 'www.google.com'],
+      ['10/2005', '2008', 'For a long time nothing happened', 'red', 'http://www.google.com'],
       ['01/2008', '05/2009', 'LOST Season #4', 'yellow', ''],
       ['01/2009', '05/2009', 'LOST Season #4', 'red', ''],
       ['02/2010', '05/2010', 'LOST Season #5', 'blue', ''],
-      ['09/2008', '06/2010', 'FRINGE #1 & #2', 'blue', '']
+      ['09/2008', '06/2010', 'Just include Timesheet.js and configure your data. No external dependencies, no jQuery ', 'blue', '']
     ]);
 
     document.querySelector('#switch-dark').addEventListener('click', function() {
