8838a8edd5acc783beb2ad32b2ccbbe4606efb18
Nikola Tucakovic <nikola@byteout.com>
2015-07-17 16:18:06 +0200

    position bubbles

--- source/javascripts/timesheet-advanced.js
+++ source/javascripts/timesheet-advanced.js
@@ -168,7 +168,7 @@
           currentBubble = currentList.bubbles[j];
           line.push(
             '<li>',
-              '<span style="left: ' + '0px; width: ' + currentBubble.getWidth(this.widthYear) + 'px;" class="bubble bubble-' + currentBubble.type + '" data-duration="' + currentBubble.monthsLength + '"></span>',
+              '<span style="left: ' + currentBubble.monthOffsetStart * this.widthYear / 12 + 'px; width: ' + currentBubble.getWidth(this.widthYear) + 'px;" class="bubble bubble-' + currentBubble.type + '" data-duration="' + currentBubble.monthsLength + '"></span>',
               '<span class="info-wrapper">',
                 '<span class="date">' + currentBubble.getDateLabel() + '</span>',
                 '<span class="label">' + currentBubble.label + '</span>',
