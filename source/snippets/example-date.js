69fca86f6d24b817376e47afddd9d7296fffe33b
Jelena <jelena@byteout.com>
2015-07-21 18:22:28 +0200

    changed examples

--- source/snippets/example-date.js
+++ source/snippets/example-date.js
@@ -1,13 +1,16 @@
-new Timesheet('timesheet-default', 'parallel', 2002, 2011, [
-  ['2002', '09/2002', 'A freaking awesome time', 'red', ''],
-  ['06/2002', '09/2003', 'Some great memories', 'blue', ''],
-  ['2003', '', 'Had very bad luck', '', ''],
-  ['10/2003', '2006', 'At least had fun', 'yellow', ''],
-  ['02/2005', '05/2006', 'Enjoyed those times as well', 'green', '#'],
-  ['07/2005', '09/2005', 'Bad luck again', 'purple', '#'],
-  ['10/2005', '2008', 'For a long time nothing happened', 'red', 'http://www.google.com'],
-  ['01/2008', '05/2009', 'LOST Season #4', 'yellow', ''],
-  ['01/2009', '05/2009', 'LOST Season #4', 'red', ''],
-  ['02/2010', '05/2010', 'LOST Season #5', 'blue', ''],
-  ['09/2008', '06/2010', 'Just include Timesheet.js and configure your data. No external dependencies, no jQuery ', 'blue', '']
-]);
+new Timesheet(
+    [
+      ['2002', '09/2002', 'First project', 'red', ''],
+      ['06/2002', '09/2003', 'Second project', 'blue', ''],
+      ['2003', '', 'Still working on', '', ''],
+      ['10/2003', '2006', 'Example project', 'yellow', 'http://www.example.com'],
+      ['02/2005', '05/2006', 'Green project', 'green', '#'],
+      ['07/2005', '09/2005', 'Most recent project', 'purple', '#']
+    ],
+    {
+      container: 'timesheet-default', // container ID
+      type: 'parallel', // can also be 'serial'
+      timesheetYearMin: 2002,
+      timesheetYearMax: 2011
+      // extraClass: try 'timesheet--white' or 'timesheet--no-date'
+    });
