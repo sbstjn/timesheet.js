a44f90e57f93abbc675fd11d4984657347767e6f
Nikola Tucakovic <nikola@byteout.com>
2016-03-22 13:55:22 +0100

    Fix landing page

--- source/snippets/example-date.js
+++ source/snippets/example-date.js
@@ -1,19 +1,16 @@
-new Timesheet(
-    [
-      ['2002', '09/2002', 'First project', 'red', ''],
-      ['06/2002', '09/2003', 'Second project', 'blue', ''],
-      ['2003', '', 'Still working on', '', ''],
-      ['10/2003', '2006', 'Yellow project', 'yellow', 'http://www.example.com'],
-      ['02/2005', '05/2006', 'Green project', 'green', '#'],
-      ['07/2005', '09/2005', 'The shortest project', 'purple', '#']
-    ],
-    {
-      container: 'timesheet-default', // container ID
-      type: 'parallel', // can also be 'serial'
-      timesheetYearMin: 2002,
-      timesheetYearMax: 2008
-      // extraClass: add custom classes
-      // theme: 'dark' or 'light'
-      // showDate: true or false - show or hide date in label
-      // scrollX: true or false - allow or prevent horizontal scroll of timesheet on mouse wheel
-    });
+var bubbles = [
+  {start: '2002',    end: '09/2002', label: 'First project', type: 'red', data: { technologies: 'php,drupal,mysql,symfony' }},
+  {start: '06/2002', end: '09/2003', label: 'Second project', type: 'blue'},
+  {start: '2003',    label: 'Still working on'},
+  {start: '10/2003', end: '2006',    label: 'Yellow project', type: 'yellow', link: 'http://www.example.com'},
+  {start: '02/2005', end: '05/2006', label: 'Green project', type: 'green', link: '#'},
+  {start: '07/2005', end: '09/2005', label: 'The shortest project', type: 'purple', link: '#'}
+];
+
+new Timesheet(bubbles, {
+  container: 'my-timesheet',
+  type: 'parallel',
+  timesheetYearMin: 2002,
+  timesheetYearMax: 2008,
+  theme: 'light'
+});
